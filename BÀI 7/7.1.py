95837d866177278f742fe774feb46da37aa66ab3
quanho1234 <quanho7905@gmail.com>
2024-11-28 15:13:53 +0700

    Add files via upload

---
+++ BÀI 7/7.1.py
@@ -0,0 +1,29 @@
+print("Sinh vien: Ho Anh Quan")
+print("Ma so SV : 235752021610088")
+print("#######################################")
+################################################
+def read_and_reverse_file(filename):
+    try:
+        
+        with open(filename, 'r') as file:
+            
+            content = file.readlines()
+        
+        
+        reversed_content = content[::-1]
+        
+        
+        for line in reversed_content:
+            print(line.strip())  
+
+    except FileNotFoundError:
+        print(f"File '{filename}' không tồn tại.")
+    except Exception as e:
+        print(f"Có lỗi xảy ra: {e}")
+
+def main():
+    filename = input("Nhập tên file (bao gồm phần mở rộng, ví dụ: 'data.txt'): ")
+    read_and_reverse_file(filename)
+
+if __name__ == "__main__":
+    main()
---
+++ BÀI 7/7.8.py
@@ -0,0 +1,24 @@
+print("Sinh vien: Ho Anh Quan")
+print("Ma so SV : 235752021610088")
+print("#######################################")
+################################################
+def write_list_to_file(filename, data_list):
+    try:
+        
+        with open(filename, 'w', encoding='utf-8') as file:
+            for item in data_list:
+                file.write(f"{item}\n")  
+        print(f"Nội dung đã được ghi vào tệp '{filename}'.")
+    except Exception as e:
+        print(f"Có lỗi xảy ra khi ghi vào tệp: {e}")
+
+def main():
+    filename = input("Nhập tên tệp (bao gồm phần mở rộng, ví dụ: 'data.txt'): ")
+    
+ 
+    data_list = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+    
+    write_list_to_file(filename, data_list)
+
+if __name__ == "__main__":
+    main()
